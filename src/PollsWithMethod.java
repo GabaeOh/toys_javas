392679339f33c015f3f564f839987bb34b44b28e
GabaeOh <rkqo4577@gmail.com>
2023-05-31 17:59:42 +0900

    공유

---
+++ src/PollsWithMethod.java
@@ -0,0 +1,16 @@
+import polls.PolllnitailArray;
+import polls.PollScanners;
+import polls.PollStatistics;
+
+public class PollsWithMethod {
+    public static void main(String[] args) {
+        try {
+            PolllnitailArray polllnitailArray = new PolllnitailArray();
+            PollScanners pollScanners= new PollScanners();
+            PollStatistics pollStatistics = new PollStatistics();
+            
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
+}
---
+++ src/polls/PolllnitailArray.java
@@ -0,0 +1,4 @@
+package polls;
+
+public class PolllnitailArray {
+}
