40c782eb509a750c52a9c4860f02f8ccfdf5762e
HeesooHan <hhs950824@gmail.com>
2023-05-31 19:33:54 +0900

    first commit

--- src/polls/PollStatistics.java
+++ src/polls/PollStatistics.java
@@ -1,5 +1,24 @@
 package polls;
 
 public class PollStatistics {
-    
+    public int[] initialPollStatistics () {
+        int[] answers = new int[4]; 
+        return answers;
+    }
+
+    public static void main(String[] args) {
+        try {
+            String name = "";
+            PollStatistics pollStatistics= new PollStatistics();
+            int[] answers = pollStatistics.initialPollStatistics();
+            System.out.print("이름 :"+ name);
+            for (int first=0; first < answers.length; first=first+1) {
+                System.out.print(answers[first]+", ");
+            }
+            System.out.println();
+            // return 0;
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
 }
