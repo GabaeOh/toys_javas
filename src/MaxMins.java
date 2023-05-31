43b0a4a2f573460b3b84941d4d4013a6b9c1fd09
jeongjiwooon <jjwanm@naver.com>
2023-05-31 13:09:28 +0900

    commit

--- src/MaxMins.java
+++ src/MaxMins.java
@@ -9,12 +9,12 @@ public class MaxMins
             Scanner myObj = new Scanner(System.in);
             int first = 0;
             int second = 0;
-            MaxMins maxMins = new MaxMins();
+            MaxMins maxMins = new MaxMins(); // fuction 생성
             System.out.println("첫 번째 숫자를 입력하시오.");
-            first = myObj.nextInt();
+            first = myObj.nextInt(); // 변수 first의 값 입력
             System.out.println("두 번째 숫자를 입력하시오.");
-            second = myObj.nextInt();
-            int result = maxMins.Max(first, second);
+            second = myObj.nextInt(); // 변수 second의 값 입력
+            int result = maxMins.Max(first, second); // result 변수 선언 후 변수 result = funtion Max에서 계산
             System.out.println("두 수의 합은: "+result);
         }
         catch (Exception e)
@@ -29,7 +29,7 @@ public class MaxMins
         int result = 0;
         try
         {
-            result = first + second;
+            result = first + second; //결과값 result = 변수 first + second
         }
         catch (Exception e)
         {
