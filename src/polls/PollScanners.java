528cdde2bf313a76ad498821d0b51c3bc8270514
aabchyein <abcqaz369@naver.com>
2023-05-31 19:54:21 +0900

    hyein first

--- src/polls/PollScanners.java
+++ src/polls/PollScanners.java
@@ -1,5 +1,19 @@
 package polls;
 
+import java.util.Scanner;
+
 public class PollScanners {
-    
+
+    public static void main(String[] args) {
+        try {
+
+            Scanner myObj = new Scanner(System.in); // 스캐너를 이용해 값을 입력
+            int answer = 0;
+            answer = myObj.nextInt();
+            System.out.println("답) " + answer);
+
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
 }
